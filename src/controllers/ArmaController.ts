dc987143ede26d6b024c92fb6d9482691380c0c2
HenriqueCezana <cezanahenrique@gmail.com>
2026-06-17 22:38:48 -0300

    atualizando o diagrama de classe e modificando a classe ArmaController

--- src/controllers/ArmaController.ts
+++ src/controllers/ArmaController.ts
@@ -1,23 +1,54 @@
 import { Arma } from "../models/Arma";
+import type { IPesquisavel } from "../models/IPesquisavel";
 
 export class ArmaController {
-  private armas: Arma[];
+    private armas: Arma[];
 
-  constructor() {
-    this.armas = [];
-  }
+    constructor() {
+        this.armas = [];
+    }
 
-  public listarArmas(): Arma[] {
-    return this.armas;
-  }
+    public criarArma(arma: Arma): void {
+        this.armas.push(arma);
+    }
 
-  public listarArmasPorCategoria(categoria: string): Arma[] {
-    return this.armas.filter((arma) =>
-      arma.getCategoria().toLowerCase().includes(categoria.toLowerCase())
-    );
-  }
+    public listarArmas(): Arma[] {
+        return this.armas;
+    }
 
-  public buscarPorCriterio(criterio: string): Arma[] {
-    return this.armas.filter((arma) => arma.atendeCriterio(criterio));
-  }
+    public atualizarArma(uuid: string, novaArma: Arma): boolean {
+        for (let i = 0; i < this.armas.length; i++) {
+            const arma = this.armas[i];
+
+            if (arma && arma.getUuid() === uuid) {
+                this.armas[i] = novaArma;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public deletarArma(uuid: string): boolean {
+        for (let i = 0; i < this.armas.length; i++) {
+            const arma = this.armas[i];
+
+            if (arma && arma.getUuid() === uuid) {
+                this.armas.splice(i, 1);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public listarArmasPorCategoria(categoria: string): Arma[] {
+        return this.armas.filter((arma) =>
+            arma.getCategoria().toLowerCase().includes(categoria.toLowerCase())
+        );
+    }
+
+    public pesquisarPorCriterio(criterio: string): Array<IPesquisavel> {
+        return this.armas.filter((arma) => arma.atendeCriterio(criterio));
+    }
 }
